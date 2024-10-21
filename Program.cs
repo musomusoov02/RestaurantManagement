f92548a234e77b791e2977ee71111ef6a399e7e9
hp <hp@LAPTOP-335NTCS7>
2024-10-21 21:44:35 +0500

    new line

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace RestaurantManagement
 
             var builder = WebApplication.CreateBuilder(args);
 
+
             // Add services to the container.
 
             builder.Services.AddControllers();
